ff26f62fa935ffbd4fdc4ae1ac34cf0df59dbeb3
AL-Hinai <2233129a@student.gla.ac.uk>
2018-04-01 21:29:21 +0000

    Models and Databases

--- AST3R9/urls.py
+++ AST3R9/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
 from AST3R9 import views
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^AST3R9/', include('AST3R9.urls')),
     url(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^$', views.index),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from AST3R9 import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index),
 ]
